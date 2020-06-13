a73cfb13cb343b38ed6ed8c5b931d3269225952a
BillLin1209 <zzx123bill@gmail.com>
2020-06-14 00:58:00 +0800

    new project

---
+++ src/test/java/com/bill/amqp/AmqpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bill.amqp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmqpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
